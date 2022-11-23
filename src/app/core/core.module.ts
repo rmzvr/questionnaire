173f4c6ce225bfc7656eccee6325bb5f329da018
rmzvr <rmzvr@proton.me>
2022-11-23 14:31:29 +0100

    feat: add header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule],
+  exports: [HeaderComponent],
 })
-export class CoreModule { }
+export class CoreModule {}

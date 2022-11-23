4805612709c5de3d172ccfc72cfc815dc8fc4d85
rmzvr <rmzvr@proton.me>
2022-11-23 14:49:15 +0100

    feat: add login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './features/homepage/homepage.component';
+import { SigninComponent } from './features/signin/signin.component';
 
 const routes: Routes = [
-  {path: '', component: HomepageComponent}
+  { path: '', component: HomepageComponent },
+  { path: 'login', component: SigninComponent}
 ];
 
 @NgModule({
---
+++ src/app/features/signin/signin.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigninComponent } from './signin.component';
+
+
+
+@NgModule({
+  declarations: [
+    SigninComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [SigninComponent]
+})
+export class SigninModule { }

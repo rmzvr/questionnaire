7f8510fce7405bd395fc913981a20354d4d9d776
Valentyna_Olkhovska <valeskus13@gmail.com>
2022-11-23 22:26:20 +0200

    create card component, fix card style and header style

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 import { StartedButtonComponent } from './components/started-button/started-button.component';
+import { InfoCardComponent } from './components/info-card/info-card.component';
 
 @NgModule({
-  declarations: [HeaderComponent, StartedButtonComponent],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule],
-  exports: [HeaderComponent,StartedButtonComponent],
+  declarations: [HeaderComponent, StartedButtonComponent, InfoCardComponent],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule,MatCardModule],
+  exports: [HeaderComponent,StartedButtonComponent,InfoCardComponent],
 })
 export class CoreModule {}

8ed21513244df4e3ae353dc53274b6cedf52a189
rmzvr <rmzvr@proton.me>
2022-11-23 14:52:03 +0100

    feat: add register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './features/homepage/homepage.component';
 import { SigninComponent } from './features/signin/signin.component';
+import { SignupComponent } from './features/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'login', component: SigninComponent}
+  { path: 'login', component: SigninComponent },
+  { path: 'register', component: SignupComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageModule } from './features/homepage/homepage.module';
+import { SignupComponent } from './features/signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SignupComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,

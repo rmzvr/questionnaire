17df0683cf335784a080e5b24de0fe58d6fb186c
rmzvr <rmzvr@proton.me>
2022-11-24 15:10:10 +0100

    update questionnaire page

--- src/app/features/questionnaire/questionnaire.component.ts
+++ src/app/features/questionnaire/questionnaire.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { QuestionsService } from './services/questions.service';
 
 @Component({
   selector: 'app-questionnaire',
   templateUrl: './questionnaire.component.html',
-  styleUrls: ['./questionnaire.component.scss']
+  styleUrls: ['./questionnaire.component.scss'],
 })
-export class QuestionnaireComponent {
+export class QuestionnaireComponent implements OnInit {
+  public questions: any;
 
+  constructor(private questionsService: QuestionsService) {}
+
+  ngOnInit(): void {
+    if (!this.questions?.length) {
+      this.questions = this.questionsService.getQuestions();
+    }
+  }
 }

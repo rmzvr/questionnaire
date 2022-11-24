c80703f4ec3371262235204a61eb6e6c7be6237d
Valentyna_Olkhovska <valeskus13@gmail.com>
2022-11-24 18:00:08 +0200

    create footer, fix style on home page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,14 +4,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon'
 import { RouterModule } from '@angular/router';
 import { StartedButtonComponent } from './components/started-button/started-button.component';
 import { InfoCardComponent } from './components/info-card/info-card.component';
 import { SpecialistCardComponent } from './components/specialist-card/specialist-card.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, StartedButtonComponent, InfoCardComponent, SpecialistCardComponent],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule,MatCardModule],
-  exports: [HeaderComponent,StartedButtonComponent,InfoCardComponent,SpecialistCardComponent],
+  declarations: [HeaderComponent, StartedButtonComponent, InfoCardComponent, SpecialistCardComponent, FooterComponent],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule,MatCardModule,MatIconModule],
+  exports: [HeaderComponent,StartedButtonComponent,InfoCardComponent,SpecialistCardComponent,FooterComponent],
 })
 export class CoreModule {}

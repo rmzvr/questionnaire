53fa4f2754c5d008f8dc148d0aa6dfdab2ccd074
rmzvr <rmzvr@proton.me>
2022-11-24 15:11:27 +0100

    add questions service

---
+++ src/app/features/questionnaire/services/questions.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Questions } from '../mock-data/questions';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuestionsService {
+  private questions = Questions;
+  private score: number = 0;
+  public score$ = new BehaviorSubject(this.score);
+
+  public getQuestions(): any {
+    return this.questions;
+  }
+
+  public increaseScore(value: number): any {
+    this.score$.next((this.score += value));
+  }
+
+  public decreaseScore(value: number): any {
+    this.score$.next((this.score -= value));
+  }
+}

7a86f22c2d50b761f13bbbecdd82d93dfceec06b
rmzvr <rmzvr@proton.me>
2022-11-24 18:27:42 +0100

    feat: update result page

--- src/app/features/result/result.component.ts
+++ src/app/features/result/result.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from '../questionnaire/services/questions.service';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.scss']
+  styleUrls: ['./result.component.scss'],
 })
-export class ResultComponent {
+export class ResultComponent implements OnInit {
+  public score$: any;
+  constructor(private questionsService: QuestionsService) {}
 
+  ngOnInit(): void {
+    this.score$ = this.questionsService.getScore();
+  }
 }
--- src/app/features/result/result.module.ts
+++ src/app/features/result/result.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultComponent } from './result.component';
 import { ResultRoutingModule } from './result-routing.module';
+import { CoreModule } from 'src/app/core/core.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ResultComponent],
-  imports: [CommonModule, ResultRoutingModule],
+  imports: [
+    CommonModule,
+    ResultRoutingModule,
+    CoreModule,
+    MatCardModule,
+    MatButtonModule,
+    RouterModule
+  ],
   exports: [ResultComponent],
 })
 export class ResultModule {}

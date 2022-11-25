0f48f8437819b83e5f340670b4a4848d491586fa
Valentyna_Olkhovska <valeskus13@gmail.com>
2022-11-25 14:17:54 +0200

    Fix Call immediately component

--- src/app/core/components/call-menu/call-menu.component.ts
+++ src/app/core/components/call-menu/call-menu.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CallFormComponent } from '../call-form/call-form.component';
+import { ImCallFormComponent } from '../im-call-form/im-call-form.component';
+import { TherapyFormComponent } from '../therapy-form/therapy-form.component';
 
 @Component({
   selector: 'app-call-menu',
@@ -11,9 +13,21 @@ export class CallMenuComponent {
 
   constructor(
     public dialog: MatDialog
-  ){}
+  ) { }
 
-  openDialog() {
-this.dialog.open(CallFormComponent)
+  openCallDialog() {
+    this.dialog.open(CallFormComponent)
   }
+
+  openImCallDialog() {
+    this.dialog.open(ImCallFormComponent)
+  }
+
+  openTherapyDialog() {
+    this.dialog.open(TherapyFormComponent)
+  }
+
+
 }
+
+
--- src/app/core/components/im-call-form/im-call-form.component.ts
+++ src/app/core/components/im-call-form/im-call-form.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-im-call-form',
   templateUrl: './im-call-form.component.html',
   styleUrls: ['./im-call-form.component.scss']
 })
-export class ImCallFormComponent implements OnInit {
-  ngOnInit(): void {
-    console.log(1111)
+export class ImCallFormComponent {
+  constructor(
+    public dialog: MatDialogRef<ImCallFormComponent>
+  ) { }
+
+  close() {
+    this.dialog.close()
   }
+
 }

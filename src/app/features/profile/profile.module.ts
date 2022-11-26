577afe6a98ecd0e13b353abee209fec28c9a712c
Valentyna_Olkhovska <valeskus13@gmail.com>
2022-11-26 20:05:03 +0200

    profile WIP

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreModule } from 'src/app/core/core.module';
 import { ProfileComponent } from './profile.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
 
 
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    ProfileCardComponent
   ],
   imports: [
     CommonModule,

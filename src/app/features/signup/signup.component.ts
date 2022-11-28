9a6c9f8e4301a541935fe2622af65938a9b4b96b
rmzvr <rmzvr@proton.me>
2022-11-28 13:30:58 +0100

    feat: update signup page

--- src/app/features/signup/signup.component.ts
+++ src/app/features/signup/signup.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  public isPasswordHidden: boolean = true;
 
+  public registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(24),
+        Validators.pattern(/(?=.*\d)(?=.*[A-Z])((?=.*\W)|(?=.*_))^[^ ]+$/),
+      ]),
+    });
+  }
+
+  public get name(): any {
+    return this.registerForm.get('name');
+  }
+
+  public get email(): any {
+    return this.registerForm.get('email');
+  }
+
+  public get password(): any {
+    return this.registerForm.get('password');
+  }
 }
--- src/app/features/signup/signup.module.ts
+++ src/app/features/signup/signup.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup.component';
-
-
+import { CoreModule } from 'src/app/core/core.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [SignupComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CoreModule,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   exports: [SignupComponent],
 })
 export class SignupModule {}

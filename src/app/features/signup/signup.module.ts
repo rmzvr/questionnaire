188848c738e4f885fffd3cf8ff0e82ab14793398
rmzvr <rmzvr@proton.me>
2022-12-06 11:59:39 +0100

    feat: connect backend & add confirm dialog

--- src/app/features/signup/signup.module.ts
+++ src/app/features/signup/signup.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup.component';
@@ -9,9 +10,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [SignupComponent],
+  declarations: [SignupComponent, ConfirmDialogComponent],
   imports: [
     CommonModule,
     CoreModule,
@@ -22,6 +25,8 @@ import { MatIconModule } from '@angular/material/icon';
     ReactiveFormsModule,
     MatInputModule,
     MatIconModule,
+    HttpClientModule,
+    MatDialogModule,
   ],
   exports: [SignupComponent],
 })

0f64a0acac56d97b7d34654c081a88931441c2c7
Valentyna_Olkhovska <valeskus13@gmail.com>
2022-12-07 20:50:54 +0200

    format doc

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -9,17 +9,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EmailPanelComponent } from './components/email-panel/email-panel.component';
 import { PasswordPanelComponent } from './components/password-panel/password-panel.component';
 import { MatIconModule } from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { AvatarPanelComponent } from './components/avatar-panel/avatar-panel.component';
 import { AdditionalPanelComponent } from './components/additional-panel/additional-panel.component';
 import { NgxMatFileInputModule } from '@angular-material-components/file-input';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSelectCountryModule } from "@angular-material-extensions/select-country";
-import {HttpClientModule} from '@angular/common/http';
-import {MatTabsModule} from '@angular/material/tabs';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 
 
@@ -53,7 +53,7 @@ import { MatCardModule } from '@angular/material/card';
     MatSelectCountryModule.forRoot('de'),
     MatTabsModule,
     MatCardModule
-    
+
   ],
   exports: [ProfileComponent]
 })

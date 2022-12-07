33ab349ffd996e959eeb26f1c78dfcceae1c6531
Valentyna_Olkhovska <valeskus13@gmail.com>
2022-12-07 21:15:10 +0200

    create profile service

---
+++ src/app/features/profile/services/profile.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AdditionalInfo } from '../models/additional-info.model';
+import { Avatar } from '../models/avatar.model';
+import { Email } from '../models/email.model';
+import { Password } from '../models/password.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  constructor(private http: HttpClient) {}
+
+  public AddAvatar(avatar: Avatar): Observable<Avatar> {
+    return this.http.post(
+      'http://localhost:8088/...',
+      avatar
+    ) as Observable<Avatar>;
+  }
+
+  public changeEmail(email: Email): Observable<Email> {
+    return this.http.post(
+      'http://localhost:8088/...',
+      email
+    ) as Observable<Email>;
+  }
+  public changePassword(password: Password): Observable<Password> {
+    return this.http.post(
+      'http://localhost:8088/...',
+      password
+    ) as Observable<Password>;
+  }
+
+  public addAdditionalInfo(additionalInfo: AdditionalInfo): Observable<AdditionalInfo> {
+    return this.http.post(
+      'http://localhost:8088/...',
+      additionalInfo
+    ) as Observable<AdditionalInfo>;
+  }
+
+}
